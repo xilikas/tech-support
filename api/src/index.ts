f7ff9fd159594d57bf836a840de28ad3aaeb02b8
Fil Luzadas <flg.l@outlook.com>
2019-04-09 23:14:56 -0400

    Changed Nest for Loopback

---
+++ api/src/index.ts
@@ -0,0 +1,16 @@
+import {Api} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {Api};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new Api(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
